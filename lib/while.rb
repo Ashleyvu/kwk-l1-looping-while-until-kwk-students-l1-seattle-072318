81602a1ef02b3ab91cc26a191753f56428a8e1f7
Ashley Vu <ashleyvuhoo@gmail.com>
2018-07-24 22:44:49 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,7 +2,7 @@ def using_while
 	levitation_force = 6
 	while levitation_force < 10 
 	puts "Wingardium Leviosa"
-	levaitation_force += 1
+	levitation_force += 1
 	end
 	puts "Our feathers is flyin' high! Hermoine is so so proud."
 end
